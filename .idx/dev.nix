ec8159f1b9e31646fdcbe708ea5ad090dc20e3b0
vicajilau <vicajilau@gmail.com>
2024-05-05 08:28:43 +0000

    IDX Support Added

---
+++ .idx/dev.nix
@@ -0,0 +1,52 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodePackages.firebase-tools
+    pkgs.jdk17
+    pkgs.unzip
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        build-flutter = ''
+          flutter build apk --debug
+
+          # TODO: Execute web build in debug mode.
+          # flutter run does this transparently either way
+          # https://github.com/flutter/flutter/issues/96283#issuecomment-1144750411
+          # flutter build web --profile --dart-define=Dart2jsOptimization=O0 
+
+          adb -s localhost:5555 wait-for-device
+        '';
+      };
+      
+      # To run something each time the environment is rebuilt, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+          manager = "flutter";
+        };
+        android = {
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          manager = "flutter";
+        };
+      };
+    };
+  };
+}
